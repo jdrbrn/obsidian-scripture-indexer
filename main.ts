190029510414448f5770be973e224e06677399d5
jdrbrn <jdrbrn@users.noreply.github.com>
2024-08-02 16:45:59 -0400

    Initial Release

--- main.ts
+++ main.ts
@@ -1,81 +1,105 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile, ToggleComponent } from 'obsidian';
+// TODO
+// Write to files in folders for Books vs one giant file?
+const BibleBooksNameTable = new Map([
+	["Genesis", "Genesis"], ["Gen", "Genesis"],	["Gen.", "Genesis"], ["Ge", "Genesis"], ["Exodus", "Exodus"], ["Ex", "Exodus"], ["Ex.", "Exodus"],["Leviticus", "Leviticus"],
+	["Lev", "Leviticus"], ["Lev.", "Leviticus"], ["Le", "Leviticus"], ["Numbers", "Numbers"], ["Num", "Numbers"], ["Num.", "Numbers"], ["Nu", "Numbers"],
+	["Deuteronomy", "Deuteronomy"], ["Deut", "Deuteronomy"], ["Deut.", "Deuteronomy"], ["De", "Deuteronomy"], ["Joshua", "Joshua"], ["Josh","Joshua"], ["Josh.","Joshua"],
+	["Jos", "Joshua"], ["Judges", "Judges"], ["Judg", "Judges"], ["Judg.", "Judges"], ["Jg", "Judges"], ["Ruth", "Ruth"], ["Ru", "Ruth"], ["1 Samuel", "1 Samuel"],
+	["1 Sam", "1 Samuel"], ["1 Sam.", "1 Samuel"], ["1Sa", "1 Samuel"], ["2 Samuel","2 Samuel"], ["2 Sam", "2 Samuel"], ["2 Sam.", "2 Samuel"], ["2Sa", "2 Samuel"],
+	["1 Kings", "1 Kings"], ["1 Ki", "1 Kings"], ["1 Ki.", "1 Kings"], ["1Ki", "1 Kings"], ["2 Kings", "2 Kings"], ["2 Ki", "2 Kings"], ["2 Ki.", "2 Kings"], ["2Ki", "2 Kings"],
+	["1 Chronicles", "1 Chronicles"], ["1 Chron", "1 Chronicles"], ["1 Chron.", "1 Chronicles"], ["1Ch", "1 Chronicles"], ["2 Chronicles", "2 Chronicles"], ["2 Chron", "2 Chronicles"],
+	["2 Chron.", "2 Chronicles"], ["2Ch", "2 Chronicles"], ["Ezra", "Ezra"], ["Ezr", "Ezra"], ["Nehemiah", "Nehemiah"], ["Neh", "Nehemiah"], ["Neh.", "Nehemiah"], ["Ne", "Nehemiah"],
+	["Esther", "Esther"], ["Es", "Esther"], ["Job", "Job"], ["Psalms", "Psalms"], ["Psalm", "Psalms"], ["Ps", "Psalms"], ["Ps.", "Psalms"], ["Proverbs", "Proverbs"],
+	["Prov", "Proverbs"], ["Prov.", "Proverbs"], ["Pr", "Proverbs"], ["Ecclesiastes", "Ecclesiastes"], ["Eccl", "Ecclesiastes"], ["Eccl.", "Ecclesiastes"], ["Ec", "Ecclesiastes"],
+	["Song of Solomon", "Song of Solomon"], ["Song of Sol", "Song of Solomon"], ["Song of Sol.", "Song of Solomon"], ["Ca", "Song of Solomon"], ["Isaiah", "Isaiah"], 
+	["Isa", "Isaiah"], ["Isa.", "Isaiah"], ["Jeremiah", "Jeremiah"], ["Jer", "Jeremiah"], ["Jer.", "Jeremiah"], ["Lamentations", "Lamentations"], ["Lam", "Lamentations"], 
+	["Lam.", "Lamentations"], ["La", "Lamentations"], ["Ezekiel", "Ezekiel"], ["Ezek", "Ezekiel"], ["Ezek.", "Ezekiel"], ["Eze", "Ezekiel"], ["Daniel", "Daniel"], ["Dan", "Daniel"],
+	["Dan.", "Daniel"], ["Da", "Daniel"], ["Hosea", "Hosea"], ["Hos", "Hosea"], ["Hos.", "Hosea"], ["Ho", "Hosea"], ["Joel", "Joel"], ["Joe", "Joel"], ["Amos", "Amos"], ["Am", "Amos"],
+	["Obadiah", "Obadiah"], ["Obad", "Obadiah"], ["Obad.", "Obadiah"], ["Ob", "Obadiah"], ["Jonah", "Jonah"], ["Jon", "Jonah"], ["Micah", "Micah"], ["Mic", "Micah"], ["Mic.", "Micah"],
+	["Nahum", "Nahum"], ["Nah", "Nahum"], ["Nah.", "Nahum"], ["Na", "Nahum"], ["Habakkuk", "Habakkuk"], ["Hab", "Habakkuk"], ["Hab.", "Habakkuk"], ["Zephaniah", "Zephaniah"],
+	["Zeph", "Zephaniah"], ["Zeph.", "Zephaniah"], ["Zep", "Zephaniah"], ["Haggai", "Haggai"], ["Hag", "Haggai"], ["Hag.", "Haggai"], ["Zechariah", "Zechariah"], ["Zech", "Zechariah"],
+	["Zech.", "Zechariah"], ["Zec", "Zechariah"], ["Malachi", "Malachi"], ["Mal", "Malachi"], ["Mal.", "Malachi"], ["Matthew", "Matthew"], ["Matt", "Matthew"], ["Matt.", "Matthew"],
+	["Mt", "Matthew"], ["Mark", "Mark"], ["Mr", "Mark"], ["Luke", "Luke"], ["Lu", "Luke"], ["John", "John"], ["Joh", "John"], ["Acts", "Acts"], ["Ac", "Acts"], ["Romans", "Romans"],
+	["Rom", "Romans"], ["Rom.", "Romans"], ["Ro", "Romans"], ["1 Corinthians", "1 Corinthians"], ["1 Cor", "1 Corinthians"], ["1 Cor.", "1 Corinthians"], ["1Co", "1 Corinthians"],
+	["2 Corinthians", "2 Corinthians"], ["2 Cor", "2 Corinthians"], ["2 Cor.", "2 Corinthians"], ["2Co", "2 Corinthians"], ["Galatians", "Galatians"], ["Gal", "Galatians"],
+	["Gal.", "Galatians"], ["Ga", "Galatians"], ["Ephesians", "Ephesians"], ["Eph", "Ephesians"], ["Eph.", "Ephesians"], ["Philippians", "Philippians"], ["Phil", "Philippians"],
+	["Phil.", "Philippians"], ["Php", "Philippians"], ["Colossians", "Colossians"], ["Col", "Colossians"], ["Col.", "Colossians"], ["1 Thessalonians", "1 Thessalonians"],
+	["1 Thess", "1 Thessalonians"], ["1 Thess.", "1 Thessalonians"], ["1Th", "1 Thessalonians"], ["2 Thessalonians", "2 Thessalonians"], ["2 Thess", "2 Thessalonians"], 
+	["2 Thess.", "2 Thessalonians"], ["2Th", "2 Thessalonians"], ["1 Timothy", "1 Timothy"], ["1 Tim", "1 Timothy"], ["1 Tim.", "1 Timothy"], ["1Ti", "1 Timothy"],
+	["2 Timothy", "2 Timothy"], ["2 Tim", "2 Timothy"], ["2 Tim.", "2 Timothy"], ["2Ti", "2 Timothy"], ["Titus", "Titus"], ["Tit", "Titus"], ["Philemon", "Philemon"],
+	["Philem", "Philemon"], ["Philem.", "Philemon"], ["Phm", "Philemon"], ["Hebrews", "Hebrews"], ["Heb", "Hebrews"], ["Heb.", "Hebrews"], ["James", "James"], ["Jas", "James"],
+	["Jas.", "James"], ["1 Peter", "1 Peter"], ["1 Pet", "1 Peter"], ["1 Pet.", "1 Peter"], ["1Pe", "1 Peter"], ["2 Peter", "2 Peter"], ["2 Pet", "2 Peter"], ["2 Pet.", "2 Peter"],
+	["2Pe", "2 Peter"], ["1 John", "1 John"], ["1Jo", "1 John"], ["2 John", "2 John"], ["2Jo", "2 John"], ["3 John", "3 John"], ["3Jo", "3 John"], ["Jude", "Jude"],
+	["Revelation", "Revelation"], ["Rev", "Revelation"], ["Rev.", "Revelation"], ["Re", "Revelation"]
+]);
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+const BookOrder = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles",
+	"Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos",
+	"Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians",
+	"Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter",
+	"1 John", "2 John", "3 John", "Jude", "Revelation"];
+	
+interface ScriptureIndexerSettings {
+	indexFilePath: string;
+	// Book -> Chapters -> Verses -> Refs (Path to file)
+	// index of array is number of Chapter/Verse (0 index for chapter/verse is ref of whole book/chapter)
+	indexMap: Array<Array<Array<Array<String>>>>;
+	indexOnSave: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ScriptureIndexerSettings = {
+	indexFilePath: 'Index.md',
+	indexMap: [],
+	indexOnSave: false
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ScriptureIndexer extends Plugin {
+	settings: ScriptureIndexerSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'index-all-scriptures',
+			name: 'Index All Files',
 			callback: () => {
-				new SampleModal(this.app).open();
+				this.IndexAllFiles();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+		
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'index-scriptures',
+			name: 'Index This File',
+			callback: () => {
+				let curFile = this.app.workspace.getActiveFile();
+				console.log(curFile?.path);
+				if (curFile != null){
+					this.IndexFile(this.app.workspace.getActiveFile()!);
 				}
+			}
+		});
+		
+		this.addCommand({
+			id: 'reset-index',
+			name: 'Reset Index',
+			callback: () => {
+				this.settings.indexMap = [];
+				this.WriteIndex();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new ScriptureIndexerSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerEvent(this.app.vault.on('modify', (file) => {
+			if (this.settings.indexOnSave){
+				this.IndexFile(this.app.vault.getFileByPath(file.path)!);
+				console.log(this.settings.indexMap)
+				this.WriteIndex();
+			}
+		}));
 	}
 
 	onunload() {
@@ -89,28 +113,210 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async IndexAllFiles() {
+		console.log("Indexing All Files");
+		let files = await this.app.vault.getMarkdownFiles();
+		await files.forEach(file => this.IndexFile(file));
+		await this.saveSettings();
+		console.log(this.settings.indexMap);
+		this.WriteIndex();
+	}
+	
+	async IndexFile(file: TFile) {
+		if (file.path == this.settings.indexFilePath) {return;}
+		console.log("Indexing File: " + file.name);
+		let contents = await this.app.vault.cachedRead(file);
+		for (let key of BibleBooksNameTable.keys()) {
+			// (?<!\d )(((<BOOK>) )\d+([:,]\d+[-\d, ;ab]*)*) - RegEx to find references in the form <BOOK> Chapter(:Verses, Verses-Verse; Chapters:Verses) etc
+			// Build with escaping the '\' characters and passing the global flag to the constructor
+			let search = new RegExp("(?<!\\d )(((" + key + ") )\\d+([:,]\\d+[-\\d, ;ab]*)*)", "g")
+			let results = contents.match(search);
+			if (results!=null) {
+				let book = BibleBooksNameTable.get(key)!;
+				results.forEach(result => {
+					// Get rid of book (abbreviation) + then split with "; " to get seperate Chapter/Verse references
+					// Also gets rid of any trailing "; " if in a list w/ different book references in source text
+					let chapterVerseLookups = result.slice(key.length+1).split("; ");
+					chapterVerseLookups.forEach (lookup => {
+						// If no actual lookup then skip
+						if (lookup == '') {return;}
+
+						// Get chapter number
+						let chapter = lookup.split(':')[0];
+
+						// Check for only chapter ref or for Jude which has no chapters (and verses will be handled as a chapter)
+						if (lookup.split(':').length == 1)
+						{
+							// Check for the chapter being for an interval of verses and handle if needed
+							if (chapter.contains('-')){
+								let boundaries = chapter.split('-');
+								// Check if actually an interval or just a rogue "-" included in regex
+								// By checking if second verse bound is valid or NaN when parsed
+								if (Number.isNaN(parseInt(boundaries[1]))) {
+									this.AddRef(book, parseInt(chapter), 0, file);
+								} else {
+									let lowerBound = parseInt(boundaries[0]);
+									let upperBound = parseInt(boundaries[1]);
+									for (let i = lowerBound; i<=upperBound; i++)
+									{
+										this.AddRef(book, i, 0, file);
+									}
+								}
+							} else {
+								// parseInt will auto strip references to an a/b verse (ex: 25a -> 25)
+								this.AddRef(book, parseInt(chapter), 0, file);
+							}
+							return;
+						}
+
+						// Split for multiple verse lookups
+						let verseLookups = lookup.split(':')[1].split(',');
+						verseLookups.forEach(verseLookup => {
+							// Check for the lookup being for an interval of verses and handle if needed
+							if (verseLookup.contains('-')){
+								let verseBoundaries = verseLookup.split('-');
+								// Check if actually an interval or just a rogue "-" included in regex
+								// By checking if second verse bound is valid or NaN when parsed
+								if (Number.isNaN(parseInt(verseBoundaries[1]))) {
+									this.AddRef(book, parseInt(chapter), parseInt(verseBoundaries[0]), file);
+								} else {
+									let lowerBound = parseInt(verseBoundaries[0]);
+									let upperBound = parseInt(verseBoundaries[1]);
+									for (let i = lowerBound; i<=upperBound; i++)
+									{
+										this.AddRef(book, parseInt(chapter), i, file);
+									}
+								}
+							} else {
+								// parseInt will auto strip references to an a/b verse (ex: 25a -> 25)
+								this.AddRef(book, parseInt(chapter), parseInt(verseLookup), file);
+							}
+						})
+					})
+				})
+			}
+		};
+	}
+
+	AddRef(book:string, chapter:number, verse:number, ref:TFile){
+		let bookArrayIndex = BookOrder.findIndex((name) => name == book);
+		let refPath = ref.path;
+		// Chap -> Verse -> Ref
+		// 0 index of Chap/Verse is for whole book/chap
+
+		// Check if index for the book exists, if not make it
+		if (this.settings.indexMap[bookArrayIndex] == null) {
+			this.settings.indexMap[bookArrayIndex] = [];
+		}
+
+		// Check if index for the chapter exists, if not make it
+		if (this.settings.indexMap[bookArrayIndex][chapter] == null)
+		{
+			this.settings.indexMap[bookArrayIndex][chapter] = [];
+		}
+
+		// Check if index for the chapter exists, if not make it
+		if (this.settings.indexMap[bookArrayIndex][chapter][verse] == null)
+		{
+			this.settings.indexMap[bookArrayIndex][chapter][verse] = [];
+		}
+
+		// Check if index for verse already contains the reference, if not add it
+		if (!this.settings.indexMap[bookArrayIndex][chapter][verse].contains(refPath))
+		{
+			this.settings.indexMap[bookArrayIndex][chapter][verse].push(refPath);
+		}
+	}
+
+	async WriteIndex() {
+		// Get vault for quick access
+		let vault = this.app.vault;
+		// Check if indexFile and any folders on the path exist; If not create them
+		if (vault.getFileByPath(this.settings.indexFilePath)==null) {
+			let splitPath = this.settings.indexFilePath.split('/');
+			let curPath = ""
+			for (let i=0; i<splitPath.length-1; i++)
+			{
+				curPath += splitPath[i];
+				if (vault.getFolderByPath(curPath)==null) {
+					vault.createFolder(curPath);
+				}
+				curPath += "/";
+			}
+			await vault.create(this.settings.indexFilePath, "");
+		}
+
+		// Get indexFile for quick access
+		let indexFile = vault.getFileByPath(this.settings.indexFilePath)!;
+
+		// Reset indexFile
+		vault.modify(indexFile,"File is automatically generated and any changes will be overwritten\n");
+
+		// Get indexMap for quick access
+		let indexMap = this.settings.indexMap;
+
+		// Iterate through books to write indexes
+		for (let bookNum = 0; bookNum < indexMap.length; bookNum++)
+		{
+			// Check if book has any data; if not skip
+			if (indexMap[bookNum] == undefined) {continue;}
+
+			let bookName = BookOrder[bookNum];
+			// Setup output
+			let output = "### "+bookName+"\n";
+			output += ("| Verse | Ref |\n");
+			output += ("| ---- | ---- |\n");
+			// Var to count refs; only need to output an index if > 0
+			let refCount = 0;
+			// Iterate through chapters
+			for (let chapNum = 0; chapNum < indexMap[bookNum].length; chapNum++) {
+				// Check if chap has any data; if not skip
+				if (indexMap[bookNum][chapNum] == undefined) {continue;}
+
+				// Iterate through verses
+				for (let verseNum = 0; verseNum < indexMap[bookNum][chapNum].length; verseNum++)
+				{
+					// Check if verse has any data; if not skip
+					if (indexMap[bookNum][chapNum][verseNum] == undefined) {continue;}
+
+					// Iterate through references
+					for (let refNum = 0; refNum < indexMap[bookNum][chapNum][verseNum].length; refNum++){
+						// For first ref need to add the chap/verse to the table
+						// Also only needed if chap > 0 since chap of 0 is a ref to whole book not a specific chapter/verse
+						if (refNum == 0 && chapNum > 0) { 
+							output += ("| " + chapNum)
+							// Check if verse > 0 (0 means ref is for whole chapter or is for a verse in Jude which doesn't have chapters and chapters are handled as verses)
+							if (verseNum > 0) {
+								output += (":"+verseNum+" | ");
+							} else {
+								output += (" | ")
+							}
+						} else {
+							output += ("| | ");
+						}
+						// Get link to ref
+						let refPath = indexMap[bookNum][chapNum][verseNum][refNum];
+						// Get name of ref (last part of path is file name; subtract ".md" to get title)
+						let refName = refPath.split('/').last()!.slice(0,-3)
+						// Add link to output in form [[PATH|Name]]
+						output += ("[["+refPath+"\\|"+refName+"]] |\n");
+						// Inc refCount since we added a reference
+						refCount++;
+					}
+				}
+			}
+			// If we had a reference then output table to file
+			if (refCount > 0){ vault.append(indexFile, output)}
+		}
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ScriptureIndexerSettingTab extends PluginSettingTab {
+	plugin: ScriptureIndexer;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ScriptureIndexer) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,14 +327,22 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Index File Path')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.indexFilePath)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.indexFilePath = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Index on Save?')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.indexOnSave)
+				.onChange(async (val) => {
+					this.plugin.settings.indexOnSave = val;
+					await this.plugin.saveSettings();
+				})
+			)
 	}
 }
